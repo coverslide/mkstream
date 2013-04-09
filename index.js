f11abd868e897cb861a0eaf31889f35e3ba95221
Richard Hoffman <coverslide@gmail.com>
2013-04-09 10:23:29 -0700

    Add support for streams2, streams1 should still work (i hope)

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 var Stream = require('stream')
-var inherits = require('util').inherits
 
 module.exports = makeStream
 
-function makeStream(target){
-  inherits(target, Stream)
+function makeStream(target, options){
+  extend(Stream, target)
+  Stream.call(target, options)
+}
+
+makeStream.Readable = function(target, options){
+  extend(Stream.Readable, target)
+  Stream.Readable.call(target, options)
+}
+
+makeStream.Writable = function(target, options){
+  extend(Stream.Writable, target)
+  Stream.Writable.call(target, options)
+}
+
+makeStream.Duplex = function(target, options){
+  extend(Stream.Duplex, target)
+  Stream.Duplex.call(target, options)
+}
+
+makeStream.Transform = function(target, options){
+  extend(Stream.Transform, target)
+  Stream.Transform.call(target, options)
+}
+
+// non-destructively extend from base class
+function extend(src, target){
+  Object.keys(src.prototype).forEach(function(prop){
+    if(!target.prototype[prop])
+      target.prototype[prop] = src.prototype[prop]
+  })
 }
