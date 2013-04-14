32a8218340c1cf02d7491de0ff77dcc32ffa49f8
Richard Hoffman <coverslide@gmail.com>
2013-04-13 23:01:31 -0700

    Fix inheritance

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 var Stream = require('stream')
+var inherits = require('util').inherits
 
 module.exports = makeStream
 
-function makeStream(target, options){
-  extend(Stream, target)
-  Stream.call(target, options)
+function makeStream(target){
+  inherits(Stream, target)
 }
 
-makeStream.Readable = function(target, options){
-  extend(Stream.Readable, target)
-  Stream.Readable.call(target, options)
+makeStream.Readable = Stream.Readable
+makeStream.makeReadable = function(target, options){
+  inherits(Stream.Readable, target)
 }
 
-makeStream.Writable = function(target, options){
-  extend(Stream.Writable, target)
-  Stream.Writable.call(target, options)
+makeStream.Writable = Stream.Writable
+makeStream.makeWritable = function(target, options){
+  inherits(Stream.Writable, target)
 }
 
-makeStream.Duplex = function(target, options){
-  extend(Stream.Duplex, target)
-  Stream.Duplex.call(target, options)
+makeStream.Duplex = Stream.Duplex
+makeStream.makeDuplex = function(target, options){
+  inherits(Stream.Duplex, target)
 }
 
-makeStream.Transform = function(target, options){
-  extend(Stream.Transform, target)
-  Stream.Transform.call(target, options)
-}
-
-// non-destructively extend from base class
-function extend(src, target){
-  Object.keys(src.prototype).forEach(function(prop){
-    if(!target.prototype[prop])
-      target.prototype[prop] = src.prototype[prop]
-  })
+makeStream.Transform = Stream.Transform
+makeStream.makeTransform = function(target, options){
+  inherits(Stream.Transform, target)
 }
