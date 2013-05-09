b6f6b717c8b4345574471cc632ff5acf1bdce7d4
Richard Hoffman <coverslide@gmail.com>
2013-05-09 06:07:50 +0000

    Use Object.create instead of inherits

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 var Stream = require('stream')
-var inherits = require('util').inherits
 
 module.exports = makeStream
 
 function makeStream(target){
-  inherits(Stream, target)
+  target.prototype = Object.create(Stream, {constructor: {value: target}})
 }
 
 //streams2 methods added for convenience so I don't have to re-require them
 //remember to call the constructor on all instantiated objects
 makeStream.Readable = Stream.Readable
 makeStream.makeReadable = function(target, options){
-  inherits(Stream.Readable, target)
+  target.prototype = Object.create(Stream.Readable.prototype, {constructor: {value: target}})
 }
 
 makeStream.Writable = Stream.Writable
 makeStream.makeWritable = function(target, options){
-  inherits(Stream.Writable, target)
+  target.prototype = Object.create(Stream.Writable.prototype, {constructor: {value: target}})
 }
 
 makeStream.Duplex = Stream.Duplex
 makeStream.makeDuplex = function(target, options){
-  inherits(Stream.Duplex, target)
+  target.prototype = Object.create(Stream.Duplex.prototype, {constructor: {value: target}})
 }
 
 makeStream.Transform = Stream.Transform
 makeStream.makeTransform = function(target, options){
-  inherits(Stream.Transform, target)
+  target.prototype = Object.create(Stream.Transform.prototype, {constructor: {value: target}})
 }
